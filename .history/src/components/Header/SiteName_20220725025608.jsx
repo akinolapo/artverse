4bed36a6f1ec9698236ec1947496709e3a37a093
Peace Akinola <akinolapo@gmail.com>
2022-07-25 03:14:01 +0100

    WIP

--- .history/src/components/Header/SiteName_20220725025608.jsx
+++ .history/src/components/Header/SiteName_20220725025608.jsx
@@ -7,7 +7,7 @@ const SiteName = () => {
         <div className='pb-8'>
         <img className='m-auto' src={process.env.PUBLIC_URL+`assets/artverse.webp`} alt='artverse'></img>
         <h3 className='text-2xl'>Connecting artists and collectors in the metaverse</h3>
-        <img className='m-auto' src={process.env.PUBLIC_URL+`assets/artverse2.webp`} alt='artverse2'></img>
+        <img className='m-auto' src={process.env.PUBLIC_URL+`assets/artverse2.webp`} alt='artverse'></img>
         </div>
         <Searchbox/>
     </div>
